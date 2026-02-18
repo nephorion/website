2c9116297e30609dd4b4aa850e4bb292be1345aa
johan jordaan <djjordaan@gmail.com>
2026-02-18 21:29:47 +1100

    Added some cache clearing and the propper deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-gcloud storage cp --recursive build/** gs://${PROJECT_ID}
+gcloud storage rsync -r build/ gs://website-427022/
+gcloud compute url-maps invalidate-cdn-cache hydra-urlmap --path "/*" --project=central-487706
+
